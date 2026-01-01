c1469a97e8cee38de64313a4123bf8846e4509bc
Raj Manish Sahu <rajmanishsahu1121@gmail.com>
2026-01-01 14:13:03 +0000

    .

--- http/index.ts
+++ http/index.ts
@@ -1,10 +1,48 @@
 import express from 'express';
+import { SignupSchema } from './types';
+import { UserModel } from './models';
 
 const app = express();
 
-app.listen(3000);
+const port = 3000;
+app.listen(port);
+console.log(`swerver running at port ${port}`)
 
-app.get('/', (req, res) => {
-   res.send('this home page');
+app.post('/auth/signup', async (req, res) => {
+   const { success, data } = SignupSchema.safeParse(req.body);
+
+   if (!success) {
+      res.status(400).json({
+         "success": false,
+         "error": "Invalid request schema",
+      })
+   }
+   const user = await UserModel.findOne({
+      email: data?.email
+   })
+
+   if (user) {
+      res.status(400).json({
+         "success": false,
+         "error": "Email already exists"
+      })
+   }
+
+   const userDb = await UserModel.create({
+      name: data?.name,
+      email: data?.email,
+      password: data?.password,
+   })
+
+   res.json({
+      success: true,
+      data: {
+         _id: userDb._id,
+         name: userDb.email,
+         password: userDb.password,
+      }
+   })
+
+
 })
 
---
+++ http/types.ts
@@ -0,0 +1,9 @@
+
+import { z } from 'zod'
+
+export const SignupSchema = z.object({
+   name: z.string(),
+   email: z.email(),
+   password: z.string().min(6),
+   role: z.enum(['student', 'teacher'])
+})
